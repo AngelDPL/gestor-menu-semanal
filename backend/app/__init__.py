992c282ca0cd9fec69caf465f93ba0b0d5adbe83
AngelDPLL <angeld0606@gmail.com>
2026-02-26 16:07:02 +0100

    chore: configurar URLs de produccion

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,7 +8,10 @@ def create_app():
 
     db.init_app(app)
     jwt.init_app(app)
-    cors.init_app(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+    cors.init_app(app, resources={r"/api/*": {"origins": [
+    "http://localhost:5173",
+    "https://gestor-menu-semanal.vercel.app"
+]}})
 
     # Registrar rutas
     from .routes.auth import auth_bp
