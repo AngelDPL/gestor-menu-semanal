7b1ad3fe5f52c3f8e7e822742649aa7b15046ef4
AngelDPLL <angeld0606@gmail.com>
2026-03-30 19:49:48 +0200

    fix: config PostgreSQL para Render

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "fallback_jwt")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///meal_planner.db")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
